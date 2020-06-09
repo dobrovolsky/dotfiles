b9d69ab8fccb123aaff2976acbfa7d14cd0ab5f0
Dobrovolsky Bogdan <bogdan.gm24@gmail.com>
2020-06-09 15:17:40 +0300

    - change order of plugins

--- shell/plugins.zsh
+++ shell/plugins.zsh
@@ -1,17 +1,5 @@
 source $HOME/.config/dotfiles/deps/antigen/antigen.zsh
 
-antigen bundle mafredri/zsh-async
-antigen bundle sindresorhus/pure
-
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-autosuggestions
-antigen bundle lukechilds/zsh-better-npm-completion
-antigen bundle MichaelAquilina/zsh-you-should-use
-antigen bundle MichaelAquilina/zsh-auto-notify
-antigen bundle changyuheng/fz
-
-antigen bundle --loc=~/dotfiles/python/_poetry
-
 antigen use oh-my-zsh
 
 antigen bundle z
@@ -26,5 +14,17 @@ antigen bundle golang
 antigen bundle docker
 antigen bundle docker-compose
 antigen bundle httpie
+
+antigen bundle mafredri/zsh-async
+antigen bundle sindresorhus/pure
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle lukechilds/zsh-better-npm-completion
+antigen bundle MichaelAquilina/zsh-you-should-use
+antigen bundle MichaelAquilina/zsh-auto-notify
+antigen bundle changyuheng/fz
+
+antigen bundle --loc=~/dotfiles/python/_poetry
 
 antigen apply

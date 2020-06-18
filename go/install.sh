2eb5284e749713a95a2832fc225bc449620a4d7b
Dobrovolsky Bogdan <bogdan.gm24@gmail.com>
2020-06-18 12:38:02 +0300

    - add cisco vpn script - add .sensitive

--- go/install.sh
+++ go/install.sh
@@ -6,6 +6,6 @@ go get github.com/davecheney/httpstat
 # install from src
 (
     cd ~/dotfiles/src
-    go build -o ~/.dotfiles/.bin/es es.go
-    go build -o ~/.dotfiles/.bin/fs fs.go
+    go build -o ~/.dotfiles/bin/es es.go
+    go build -o ~/.dotfiles/bin/fs fs.go
 )

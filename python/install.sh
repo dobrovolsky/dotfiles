e6552c6bdb1b6edfc68db14541fb1c8032aa93c9
Dobrovolsky Bogdan <bogdan.gm24@gmail.com>
2020-08-15 12:41:53 +0300

    - add wakatime cli

--- python/install.sh
+++ python/install.sh
@@ -3,6 +3,7 @@ PYTHON_VERSION=3.8.5
 pyenv install "${PYTHON_VERSION}"
 pyenv global "${PYTHON_VERSION}"
 ~/.pyenv/versions/"${PYTHON_VERSION}"/bin/pip install -r ~/.dotfiles/python/requirments.txt
+pyenv rehash
 
 # setup virtualenvs in project for poetry
 ~/.pyenv/shims/poetry config virtualenvs.in-project true

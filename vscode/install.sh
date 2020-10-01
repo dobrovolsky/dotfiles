6b1c102ae52f5c48e184ef5aa6561c43b69e3bdb
Dobrovolsky Bogdan <bogdan.gm24@gmail.com>
2020-10-01 19:09:14 +0300

    update karabiner and vscode settings

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,16 +1,12 @@
-# Themes
+code --install-extension akamud.vscode-theme-onedark
 code --install-extension akamud.vscode-theme-onelight
-code --install-extension PKief.material-icon-theme
-code --install-extension rexebin.darkpurple
-
-# Extensions
-code --install-extension vscodevim.vim
 code --install-extension esbenp.prettier-vscode
-code --install-extension foam.foam-vscode
+code --install-extension Gruntfuggly.todo-tree
 code --install-extension kortina.vscode-markdown-notes
-code --install-extension tchayen.markdown-links
-code --install-extension yzhang.markdown-all-in-one
-code --install-extension WakaTime.vscode-wakatime
+code --install-extension PKief.material-icon-theme
+code --install-extension skn0tt.markdown-link-expander
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension streetsidesoftware.code-spell-checker-ukrainian
-code --install-extension skn0tt.markdown-link-expander
+code --install-extension vscodevim.vim
+code --install-extension WakaTime.vscode-wakatime
+code --install-extension yzhang.markdown-all-in-one
